bf6c1214da97b391c52c2d787802c84245e4464d
Adam Messaoudi <adam.messaoudi@laplateforme.io>
2025-08-08 14:32:22 +0200

    Job 3: Gestion des secrets et variables d'environnement, + script check_app avec exit 0

--- app/check_app.sh
+++ app/check_app.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-# Active le mode strict : exit si une commande échoue
 set -e
 
 echo "=== Début de la vérification de l'application ==="
 
-# Vérifie si le fichier index.html existe
-if [ -f "app/index.html" ]; then
-    echo "[OK] index.html trouvé."
-    exit 0
-else
-    echo "[ERREUR] index.html manquant."
-    exit 1
-fi
+# Forcer une erreur
+exit 1
 
 echo "=== Fin de la vérification ==="
