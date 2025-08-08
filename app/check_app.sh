b4cadd59c60559ffc91d3288c6f370600e65a482
Adam Messaoudi <adam.messaoudi@laplateforme.io>
2025-08-08 14:34:56 +0200

    Job 3:Remettre le script en état de succès Après le test d’erreur, remets exit 0 dans ton script app/check_app.sh :

--- app/check_app.sh
+++ app/check_app.sh
@@ -3,7 +3,13 @@ set -e
 
 echo "=== Début de la vérification de l'application ==="
 
-# Forcer une erreur
-exit 1
+# Vérifie si index.html existe
+if [ -f "app/index.html" ]; then
+  echo "[OK] index.html trouvé."
+  exit 0
+else
+  echo "[ERREUR] index.html manquant."
+  exit 1
+fi
 
 echo "=== Fin de la vérification ==="
